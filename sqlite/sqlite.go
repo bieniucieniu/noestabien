37d0ab65da4a1c009ba5054722ace6215ff41df9
bieniucieniu <mikibien@outlook.com>
2023-12-01 11:40:04 +0100

    simplfy funcs

--- sqlite/sqlite.go
+++ sqlite/sqlite.go
@@ -2,7 +2,6 @@ package sqlite
 
 import (
 	"fmt"
-	"log"
 	"math/rand"
 
 	"github.com/bieniucieniu/noestabien/auth"
@@ -28,6 +27,13 @@ func New(dsn string) (*Database, error) {
 	return &db, nil
 }
 
+// add user, id always omited
+func (db *Database) AddUser(u *User) (*User, error) {
+	user := new(User)
+	db.sqlx.QueryRowx("INSERT INTO user (id, key, name) values ($1, $2, $3) RETURNING *;", nil, u.Key, u.Name).StructScan(user)
+	return user, nil
+}
+
 func (db *Database) CreateUser(name string) (*User, error) {
 	if l := len(name); l > 20 {
 		return nil, fmt.Errorf("to long name")
@@ -47,13 +53,7 @@ func (db *Database) CreateUser(name string) (*User, error) {
 	return user, nil
 }
 
-// add user id always omited
-func (db *Database) AddUser(u *User) (*User, error) {
-	user := new(User)
-	db.sqlx.QueryRowx("INSERT INTO user (id, key, name) values ($1, $2, $3) RETURNING *;", nil, u.Key, u.Name).StructScan(user)
-	return user, nil
-}
-
+// get user, require id or key on user
 func (db *Database) GetUser(u *User) (*User, error) {
 	user := User{}
 	if u.Id != 0 {
@@ -86,13 +86,7 @@ func (db *Database) GetUserWithToken(tokenString *string) (*User, error) {
 		return nil, fmt.Errorf("no id in token")
 	}
 
-	user, err := db.GetUser(&User{
+	return db.GetUser(&User{
 		Id: int64(idStr),
 	})
-	if err != nil {
-		log.Printf("error %s", err.Error())
-		return nil, err
-	}
-
-	return user, nil
 }
